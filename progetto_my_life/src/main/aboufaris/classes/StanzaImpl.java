7668d20c2e3ffbbcf2dd8d4608f5664e9864c8f2
nissan345 <nasrine204@gmail.com>
2025-09-24 20:12:38 +0200

    Fine implementazione classe Stanza
    Manca l'aggiunta dei commenti e vari raffinamenti

--- progetto_my_life/src/main/aboufaris/classes/StanzaImpl.java
+++ progetto_my_life/src/main/aboufaris/classes/StanzaImpl.java
@@ -6,14 +6,51 @@ import main.giuseppetti.interfaces.NPC;
 import main.neri.interfaces.OggettoGioco;
 
 public class StanzaImpl implements Stanza{
-    List<NPC> npcInStanza;               // Indica gli oggetti presenti nella stanza
-    List<OggettoGioco> oggettiInStanza;  // Indica gli NPC presenti nella stanza
+    private final List<NPC> npcInStanza;               // Indica gli oggetti presenti nella stanza
+    private final List<OggettoGioco> oggettiInStanza;  // Indica gli NPC presenti nella stanza
 
     public StanzaImpl(List<NPC> npc, List<OggettoGioco> oggetti){
         this.npcInStanza = npc;
         this.oggettiInStanza = oggetti;
     }
 
+    public List<OggettoGioco> getOggettiInStanza() {
+        return oggettiInStanza;
+    }
+
+    public List<NPC> getNpcInStanza() {
+        return npcInStanza;
+    }
+
+    public boolean hasNpc(NPC n){
+        return this.npcInStanza.contains(n);
+    }
+
+    public boolean hasOggettoStanza(OggettoGioco o){
+        return this.oggettiInStanza.contains(o);
+    }
     
-    
+    public void addNpc(NPC n) throws IllegalArgumentException{
+        if(hasNpc(n)){
+            throw new IllegalArgumentException();
+        }
+        this.npcInStanza.add(n);
+    };
+    public void addOggettoStanza(OggettoGioco o)throws IllegalArgumentException{
+        if(hasOggettoStanza(o))
+            throw new IllegalArgumentException();
+        this.oggettiInStanza.add(o);
+    };
+    public void removeOggettoStanza(OggettoGioco o)throws IllegalArgumentException{
+        if(!hasOggettoStanza(o))
+            throw new IllegalArgumentException();
+        this.oggettiInStanza.remove(o);
+    };
+
+    public void removeNpc(NPC n)throws IllegalArgumentException{
+        if(!hasNpc(n)){
+            throw new IllegalArgumentException();
+        }
+        this.npcInStanza.remove(n);
+    };
 }
--- progetto_my_life/src/main/aboufaris/interfaces/Stanza.java
+++ progetto_my_life/src/main/aboufaris/interfaces/Stanza.java
@@ -1,5 +1,25 @@
 package main.aboufaris.interfaces;
 
+import java.util.List;
+import main.giuseppetti.interfaces.NPC;
+import main.neri.interfaces.OggettoGioco;
+
 public interface Stanza{
+    /* */
+    public List<OggettoGioco> getOggettiInStanza();
+    /* */
+    public List<NPC> getNpcInStanza();
+    /* */
+    public boolean hasNpc(NPC n);
+    /* */
+    public boolean hasOggettoStanza(OggettoGioco o);
+    /* */
+    public void addNpc(NPC n);
+    /* */
+    public void addOggettoStanza(OggettoGioco o);
+    /* */
+    public void removeOggettoStanza(OggettoGioco o);
+    /* */
+    public void removeNpc(NPC n);
     
 }

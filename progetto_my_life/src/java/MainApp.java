8bc0ca8c68833ae8ba7a1232d5a4910e69866db5
Alicue10 <a.neri14@campus.uniurb.it>
2025-11-08 18:06:48 +0100

    modifica classpath e cancellazione cartella java

--- progetto_my_life/src/java/MainApp.java
+++
@@ -1,9 +0,0 @@
-
-import main.control.Control;
-public class MainApp{
-
-	public static void main(String[] args) {
-		final Control controller = Control.getControlInstance();
-		controller.startGame();
-	}
-}

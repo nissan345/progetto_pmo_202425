d1df314b568f59f263280766462372e97850bc24
Alicue10 <a.neri14@campus.uniurb.it>
2025-11-08 18:09:10 +0100

    Revert "comit precedente parte 2"
    This reverts commit d204d90e5e5841f06a7ff2c59300d1f9a3d0ba80.

--- progetto_my_life/src/MainApp.java
+++
@@ -1,9 +0,0 @@
-
-import java.main.control.Control;
-public class MainApp{
-
-	public static void main(String[] args) {
-		final Control controller = Control.getControlInstance();
-		controller.startGame();
-	}
-}

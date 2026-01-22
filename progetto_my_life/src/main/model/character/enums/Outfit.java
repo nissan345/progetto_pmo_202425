1b3ec497974ae26cf94e8f569d426249585810d4
Alicue10 <a.neri14@campus.uniurb.it>
2026-01-22 09:56:20 +0100

    corrrezione outfit

--- progetto_my_life/src/main/model/character/enums/Outfit.java
+++ progetto_my_life/src/main/model/character/enums/Outfit.java
@@ -4,22 +4,10 @@ package main.model.character.enums;
  * The Outfit enum represents different clothing styles for the main character.
  */
 
-/**
- * The Outfit enum represents different clothing styles for the main character.
- */
-
 public enum Outfit {
-<<<<<<< HEAD:progetto_my_life/src/main/fabbri/classes/Outfit.java
-<<<<<<< Updated upstream:progetto_my_life/src/main/fabbri/classes/Outfit.java
-    // ALTERNATIVE DI VESTITI
-=======
-    // ALTERNATIVES FOR CLOTHES ----------------------------------------------------------
->>>>>>> Stashed changes:progetto_my_life/src/model/character/Outfit.java
-=======
 
     // ALTERNATIVES FOR CLOTHES ----------------------------------------------------------
 
->>>>>>> main:progetto_my_life/src/main/model/character/Outfit.java
     CASUAL("Informale", "Abbigliamento casual per tutti i giorni"),
     FORMAL("Formale", "Abbigliamento elegante per occasioni speciali"),
     SPORTY("Sportivo", "Abbigliamento comodo per attività fisica"),
@@ -27,28 +15,12 @@ public enum Outfit {
     SUMMERLY("Estivo", "Abbigliamento leggero per il caldo"),
     WINTERLY("Invernale", "Abbigliamento caldo per il freddo"); 
 
-<<<<<<< HEAD:progetto_my_life/src/main/fabbri/classes/Outfit.java
-<<<<<<< Updated upstream:progetto_my_life/src/main/fabbri/classes/Outfit.java
-    // ATTRIBUTI
-    private final String name;
-    private final String description;
-
-    // COSTRUTTORE
-=======
-    // ATTRIBUTES -----------------------------------------------------------------------
-    private final String name;
-    private final String description;
-
-    // CONSTRUCTOR ----------------------------------------------------------------------
->>>>>>> Stashed changes:progetto_my_life/src/model/character/Outfit.java
-=======
 
     // ATTRIBUTES -----------------------------------------------------------------------
     private final String name;
     private final String description;
 
     // CONSTRUCTOR ----------------------------------------------------------------------
->>>>>>> main:progetto_my_life/src/main/model/character/Outfit.java
     Outfit(String name, String description) {
         this.name = name;
         this.description = description;
